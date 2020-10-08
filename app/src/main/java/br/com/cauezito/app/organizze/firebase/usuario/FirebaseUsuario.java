6c1ea1ec60d2feec0f376591f2de99d0bfbd656a
Cauê Santos <caah.sfc@live.com>
2020-10-08 18:36:08 -0300

    Lançando exceções para senha fraca, usuário já existente e formato incorreto de e-mail quando o usuário tenta se registrar

--- app/src/main/java/br/com/cauezito/app/organizze/firebase/usuario/FirebaseUsuario.java
+++ app/src/main/java/br/com/cauezito/app/organizze/firebase/usuario/FirebaseUsuario.java
@@ -1,6 +1,7 @@
 package br.com.cauezito.app.organizze.firebase.usuario;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -10,8 +11,12 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import br.com.cauezito.app.organizze.activity.CadastroActivity;
+import br.com.cauezito.app.organizze.activity.LoginActivity;
 import br.com.cauezito.app.organizze.firebase.config.FirebaseConfig;
 
 public class FirebaseUsuario {
@@ -24,14 +29,32 @@ public class FirebaseUsuario {
     }
 
     public void cadastrarUsuario(String email, String senha){
+
         autenticacao.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(activity,
                 new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) Toast.makeText(activity, "Cadastro realizado com sucesso! :)", Toast.LENGTH_LONG).show();
-                        else Toast.makeText(activity, "Erro ao concluir cadastro", Toast.LENGTH_LONG).show();
+                        if (task.isSuccessful()){
+                            Toast.makeText(activity, "Cadastro realizado com sucesso! :)", Toast.LENGTH_LONG).show();
+                            activity.startActivity(new Intent(activity, LoginActivity.class));
+                        } else {
+                            String excecao = "";
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e) {
+                                excecao = "Digite uma senha mais forte";
+                            } catch (FirebaseAuthInvalidCredentialsException e2){
+                                excecao = "Digite um e-mail válido";
+                            } catch (FirebaseAuthUserCollisionException e3){
+                                excecao = "Essa conta já existe";
+                            } catch (Exception e){
+                                excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                                e.printStackTrace();
+                            }
+
+                            Toast.makeText(activity, excecao, Toast.LENGTH_LONG).show();
+                        }
                     }
                 });
-
     }
 }
