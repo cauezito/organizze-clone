040eb008b0691e0c37919560553a2be96a870833
Cauê Santos <caah.sfc@live.com>
2020-10-12 15:27:33 -0300

    Atualizando receita total ao criar uma nova entrada

--- app/src/main/java/br/com/cauezito/app/organizze/firebase/movimentacao/entrada/GerenciaEntrada.java
+++ app/src/main/java/br/com/cauezito/app/organizze/firebase/movimentacao/entrada/GerenciaEntrada.java
@@ -8,19 +8,31 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.cauezito.app.organizze.activity.DespesaActivity;
 import br.com.cauezito.app.organizze.activity.EntradaActivity;
 import br.com.cauezito.app.organizze.firebase.config.FirebaseConfig;
+import br.com.cauezito.app.organizze.firebase.movimentacao.despesa.GerenciaDespesa;
 import br.com.cauezito.app.organizze.model.Movimentacao;
+import br.com.cauezito.app.organizze.model.Usuario;
 import br.com.cauezito.app.organizze.utils.Base64Custom;
 import br.com.cauezito.app.organizze.utils.DateCustom;
 
 public class GerenciaEntrada implements IGerenciaEntrada {
-    private DatabaseReference banco;
-    private FirebaseAuth autenticacao;
+
+    private static FirebaseAuth autenticacao;
+    private static Double receitaTotal = 0D;
+    private static DatabaseReference banco;
     private Activity entradaActivity;
+    private Double receitaAtualizada;
+
+    private static String RECEITA_TOTAL = "receitaTotal";
+    private static String USUARIOS = "usuarios";
+    private static String MOVIMENTACAO = "movimentacao";
 
     public GerenciaEntrada(Activity activity) {
         this.banco = FirebaseConfig.getDatabaseReference();
@@ -38,6 +50,9 @@ public class GerenciaEntrada implements IGerenciaEntrada {
                 entradaActivity, new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
+                        receitaAtualizada = movimentacao.getValor() + GerenciaEntrada.recuperaReceitaTotal();
+                        atualizaReceita(receitaAtualizada);
+
                         Toast.makeText(entradaActivity.getBaseContext(), "Entrada salva", Toast.LENGTH_LONG).show();
                         EntradaActivity.limpaCampos();
                     }
@@ -45,10 +60,32 @@ public class GerenciaEntrada implements IGerenciaEntrada {
         ).addOnFailureListener(entradaActivity, new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(entradaActivity.getBaseContext(), "Não foi possível salvar a despesa", Toast.LENGTH_LONG).show();
+                        Toast.makeText(entradaActivity.getBaseContext(), "Não foi possível salvar a entraad", Toast.LENGTH_LONG).show();
                     }
                 }
         );
+    }
+
+    public static Double recuperaReceitaTotal() {
+        String idUsuario = Base64Custom.codificaBase64(autenticacao.getCurrentUser().getEmail());
+
+        banco.child(USUARIOS).child(idUsuario).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario usuario = dataSnapshot.getValue(Usuario.class);
+                receitaTotal = usuario.getReceitaTotal();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
+
+        return receitaTotal;
+    }
+
+    public void atualizaReceita(Double despesa){
+        String idUsuario = Base64Custom.codificaBase64(autenticacao.getCurrentUser().getEmail());
+        banco.child(USUARIOS).child(idUsuario).child(RECEITA_TOTAL).setValue(despesa);
     }
 }
 
